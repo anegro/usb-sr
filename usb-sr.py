fb247d9691349c1f838c72719b0de2552108183b
Antonio Negro <anegro.floss@gmail.com>
2016-05-05 20:16:42 +0200

    Unmount working

--- usb-sr.py
+++ usb-sr.py
@@ -25,58 +25,93 @@
 import gobject
 import gtk
 import appindicator
-import os
+import subprocess
+import pynotify
 
-APPINDICATOR_ID = 'usb-sr'
+APP_NAME = 'usb-sr'
 ICON_NAME = 'distributor-logo'
 
+
+
 # Actions to be taken when pressing certain menu entries
-def action1_clicked(widget, data=None):
-    print "Lanzando gedit..."
-    os.system("gedit &")
+def unmount_clicked(widget, data=None):
+	print "Unmounting %s ..." % data
+	notify_begin_unmount.show()
+	
+	part = data
+	dev = data.rstrip('1234567890')
+	
+	subprocess.call('udisksctl unmount --block-device %s' % part, shell=True)
+	subprocess.call('udisksctl power-off --block-device %s' % dev, shell=True)
+
+	notify_begin_unmount.close()
+	notify_end_unmount.show()
+
 
 def quit(widget, data=None):
-    gtk.main_quit()
+	gtk.main_quit()
+
+def build_menu(indicator):
+	# Create menu
+	menu = gtk.Menu()
+
+	# Get mounted devices
+	mount_devices = subprocess.check_output('mount -l | grep -E ".* on /media/$USER.*" | cut -d " " -f 1', shell=True)
+	mount_names = subprocess.check_output('mount -l | grep -E ".* on /media/$USER.*" | grep -E -o "\[.*\]" | tr -d "[]"', shell=True)
+	mounts = dict(zip(mount_names.splitlines(), mount_devices.splitlines()))
+	
+	print mounts ###
+
+	# Add menu elements
+	for name in mounts:
+		item = gtk.MenuItem(name)
+		item.connect("activate", unmount_clicked, mounts[name])
+		item.show()
+		menu.append(item)
+ 
+	#check = gtk.CheckMenuItem("Check Menu Item")
+	#check.show();
+	#menu.append(check)
+
+	#radio = gtk.RadioMenuItem(None, "Radio Menu Item")
+	#radio.show()
+	#menu.append(radio)
+
+	separator = gtk.SeparatorMenuItem()
+	separator.show()
+	menu.append(separator)
+
+	item_exit = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+	item_exit.connect("activate", quit)
+	item_exit.show()
+	menu.append(item_exit)
+
+	indicator.set_menu(menu)
+
 
 # Main code
 if __name__ == "__main__":
 
-    # Create indicator
-    indicator = appindicator.Indicator (APPINDICATOR_ID, ICON_NAME, appindicator.CATEGORY_APPLICATION_STATUS)
-    indicator.set_status (appindicator.STATUS_ACTIVE)
+	# Create indicator
+	indicator = appindicator.Indicator (APP_NAME, ICON_NAME, appindicator.CATEGORY_APPLICATION_STATUS)
+	indicator.set_status (appindicator.STATUS_ACTIVE)
 
-    # Indicator icons
-    #ind.set_icon("indicator-messages")
-    #ind.set_attention_icon("indicator-messages-new")
+	# Indicator icons
+	#ind.set_icon("indicator-messages")
+	#ind.set_attention_icon("indicator-messages-new")
 
-    # Create menu
-    menu = gtk.Menu()
+	# Set indicator menu
+	build_menu(indicator)
 
-    # Add menu elements
-    item = gtk.MenuItem("Launch GEDIT...")
-    item.connect("activate", action1_clicked)
-    item.show()
-    menu.append(item)
- 
-    check = gtk.CheckMenuItem("Check Menu Item")
-    check.show();
-    menu.append(check)
+	# Create notifications
+	if pynotify.init(APP_NAME):
+		notify_begin_unmount = pynotify.Notification("Unmounting device", "Please wait...")
+		notify_begin_unmount.set_timeout(600)
 
-    radio = gtk.RadioMenuItem(None, "Radio Menu Item")
-    radio.show()
-    menu.append(radio)
+		notify_end_unmount = pynotify.Notification("Device unmounted", "Now you can safely remove it")
+		notify_end_unmount.set_timeout(60)
+	else:
+		print "there was a problem initializing the pynotify module"
 
-    separator = gtk.SeparatorMenuItem()
-    separator.show()
-    menu.append(separator)
-
-    item_exit = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-    item_exit.connect("activate", quit)
-    item_exit.show()
-    menu.append(item_exit)
-
-    # Set indicator menu
-    indicator.set_menu(menu)
-
-    # Run main code
-    gtk.main()
+	# Run main code
+	gtk.main()
